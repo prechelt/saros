13e57e721514dfeab46b9fdd17ff8c44aef0f1d4
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2020-01-21 13:05:45 +0100

    [STF] Use categorized stf execution

---
+++ stf.test/test/saros/stf/test/categories/FailingTests.java
@@ -0,0 +1,6 @@
+package saros.stf.test.categories;
+
+/** Marks tests are always failing and have to be fixed! */
+public interface FailingTests {
+  /* category marker */
+}
---
+++ stf.test/test/saros/stf/test/categories/FlakyTests.java
@@ -0,0 +1,6 @@
+package saros.stf.test.categories;
+
+/** Marks tests that are sometimes failing and have to be fixed! */
+public interface FlakyTests {
+  /* category marker */
+}
